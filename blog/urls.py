fddbaa35713930bd40a251129120dac95fe22351
410178050@qq.com <qqstyle21>
2018-01-30 17:54:34 +0800

    v2018-01-30

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-      url(r'^$', views.index,name='index'),
+      url(r'^$', views.IndexView.as_view(),name='index'),
       url(r'^post/(?P<id>\d+)', views.post,name='post'),
-       url(r'^tags/(?P<id>\d+)', views.tags,name='tags'),
+      url(r'^tags/(?P<id>\d+)', views.tags,name='tags'),
+      url(r'^category/(?P<id>\d+)', views.category,name='category'),
       url(r'^login', views.login,name='login'),
       url(r'^logout', views.logout,name='logout'),
 ]
