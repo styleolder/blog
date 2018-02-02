cd9b52ae221fbaefeb77ed853c90e606681bf9bf
410178050@qq.com <qqstyle21>
2018-02-02 17:29:18 +0800

    tag:2018-02-02

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ app_name = 'blog'
 
 urlpatterns = [
       url(r'^$', views.IndexView.as_view(),name='index'),
-      url(r'^post/(?P<id>\d+)', views.post,name='post'),
+      url(r'^post/(?P<pk>[0-9]+)/$', views.PostView.as_view(),name='post'),
       url(r'^tags/(?P<id>\d+)', views.tags,name='tags'),
       url(r'^category/(?P<id>\d+)', views.category,name='category'),
       url(r'^login', views.login,name='login'),
