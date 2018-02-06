71392ca03d3f0c7e26c67f30fe1b60f34d20c678
410178050@qq.com <qqstyle21>
2018-02-06 17:52:08 +0800

    添加类视图，文章tag进行了分页展示，现在可以选用户所发表的所有文章

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,10 @@ app_name = 'blog'
 urlpatterns = [
       url(r'^$', views.IndexView.as_view(),name='index'),
       url(r'^post/(?P<pk>[0-9]+)/$', views.PostView.as_view(),name='post'),
-      url(r'^tags/(?P<id>\d+)', views.tags,name='tags'),
-      url(r'^category/(?P<id>\d+)', views.category,name='category'),
+      url(r'^category/(?P<pk>[0-9]+)', views.CategoryView.as_view(),name='category'),
       url(r'^login', views.login,name='login'),
       url(r'^logout', views.logout,name='logout'),
       url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.ArchivesView.as_view(), name='archives'),
+      url(r'^tags/(?P<pk>[0-9]+)', views.TagsView.as_view(),name='tags'),
+      url(r'^user/(?P<pk>[0-9]+)', views.UserView.as_view(),name='user'),
 ]
