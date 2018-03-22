57c270f29ece28d9a4bbc4f15868072632f1f03c
410178050@qq.com <qqstyle21>
2018-03-22 17:32:27 +0800

    v2018-03-22优化评论样式v1

--- comments/views.py
+++ comments/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required(login_url="/blog/user_login")
 def sub_comment(request):
     pass
 
